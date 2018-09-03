97bb5904580b6dba6d3ac159c9f836c2f8a3bbe4
Parul Pandey <parul.parulpandey123@gmail.com>
2018-09-03 11:26:24 +0100

    fourth commit ...enum

--- parul_java_practice/src/music_synthesizer/NoteName.java
+++ parul_java_practice/src/music_synthesizer/NoteName.java
@@ -5,6 +5,56 @@ import java.util.List;
 
 public class NoteName {
 	
+	
+	/*enum NoteName {
+	C("C"),
+	CSHARP("C#"),
+	D("D"),
+	DSHARP("D#"),
+	E("E"),
+	F("F"),
+	FSHARP("F#"),
+	G("G"),
+	GSHARP("G#"),
+	A("A"),
+	ASHARP("A#"),
+	B("B");
+	
+	private final String representation;
+	NoteName(String representation) {
+		this.representation = representation;
+	}
+	
+	private String representation() {
+		return representation;
+	}
+	
+	public String toString() {
+		return representation;
+	}
+}
+
+enum NoteValue {
+	WHOLE("1"),
+	HALF("1/2"),
+	QUARTER("1/4"),
+	EIGHTH("1/8"),
+	SIXTEENTH("1/16");
+	
+	private final String representation;
+	NoteValue(String representation) {
+		this.representation = representation;
+	}
+	
+	private String representation() {
+		return representation;
+	}
+	
+	public String toString() {
+		return representation;
+	}
+}
+*/
 
 	private static List<String> noteSequence = Arrays.asList("C", "C#", "D", "D#", "E", "F",
 												"F#", "G", "G#", "A", "A#", "B");
--- parul_java_practice/src/music_synthesizer/Synthesizer.java
+++ parul_java_practice/src/music_synthesizer/Synthesizer.java
@@ -4,7 +4,7 @@ import java.beans.FeatureDescriptor;
 
 public class Synthesizer {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args)throws java.lang.Exception {
 		
 		Tunes frereJaques = new PhysicalTune();
 		
